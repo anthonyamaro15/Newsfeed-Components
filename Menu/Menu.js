c00a9f1daddc33779096c9cbc08463a6d58e6d5b
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-03-11 14:18:36 -0700

    finished with the MVP

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,12 +1,12 @@
 /* This is the data we will be using, study it but don't change anything, yet. */
 
 let menuItems = [
-  'Students',
-  'Faculty',
+  "Students",
+  "Faculty",
   "What's New",
-  'Tech Trends',
-  'Music',
-  'Log Out'
+  "Tech Trends",
+  "Music",
+  "Log Out"
 ];
 
 /* 
@@ -33,3 +33,25 @@ let menuItems = [
   Step 6: add the menu component to the DOM.
   
 */
+const mainMenu = document.querySelector(".menu-button");
+const header = document.querySelector(".header");
+
+function createMenu(arr) {
+  const menu = document.createElement("div");
+  const ul = document.createElement("ul");
+
+  menu.classList.add("menu");
+  menu.appendChild(ul);
+  arr.forEach(li => {
+    const single = document.createElement("li");
+    ul.appendChild(single);
+    single.textContent = li;
+  });
+  mainMenu.addEventListener("click", () => {
+    menu.classList.toggle("menu--open");
+  });
+
+  return menu;
+}
+
+header.appendChild(createMenu(menuItems));
